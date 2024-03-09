8c867786b57975db70ce3d15f482a8430eea3f66
George Yu <george@think.digital>
2024-03-09 13:49:24 +1030

    Finish designs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react"
 import Main from "./components/Main"
 import Navbar from "./components/Navbar"
+import './styles.css';
+
 
 function App(){
     return (
-        <div className="container">
-            <Main />
-            <Navbar />
-        </div> 
+        <div>
+            <div className="container">
+                <Navbar />
+                <Main />  
+            </div> 
+            <div className="attribution">
+                Coded by <a target="_blank" href="https://www.linkedin.com/in/george-yu-a6800a227/">George Yu🐱</a>.
+            </div>
+        </div>  
     )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,16 @@
 import React from "react"
+import Logo from "../images/react-1-logo-svg-vector.svg"
 
 function Navbar(){
     return (
-        <h1>This is Navbar</h1>
+        <nav className="navbar">
+            <div id="left">
+                <img src={Logo} alt="react logo" width="40px"/>
+                <h1>ReactFacts</h1>
+            </div>
+            <h3>ReactCourse - Project 1</h3>
+        </nav>
+        
     )
 }
 
